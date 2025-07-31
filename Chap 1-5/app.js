a16ae939d2c7e32d463bded06b47ab67481a8346
Muskaankamran <muskaankamran494@gmail.com>
2025-07-31 08:40:48 -0700

    half complete

---
+++ Chap 1-5/app.js
@@ -0,0 +1,53 @@
+//_____________________QNO 1
+
+alert("Welcome!");
+//____________________QNO 2
+
+ alert("Error please enter a valid password")
+
+//_____________________QNO 3
+
+alert("Welcome to JS Land \n Happy Coding!");
+
+//_____________________QNO 4
+alert("Welcome to JS Land..")
+alert("Happy Coding ! \nPrevent this page from creating additional dialogs")
+
+//____________________QNO 5
+alert("Hello...I can run JS thorugh my web browser's console");
+
+ //___________________QNO 6
+
+ //_______IN index.html file
+
+
+
+//__________CHAPTER 02______________
+
+//____________________QNO 1
+var username;
+//___________________QNO 2
+var myName = "Muskaankamran"
+alert(myName);
+//_________________QNO 3
+
+// a) Declare a JS variable, titled message.
+let message;
+
+// b) Assign “Hello World” to variable message
+message = "Hello World";
+
+// c) Display the message in alert box.
+alert(message);
+
+//_____________________QNO 4
+var studentName = "Maira"
+var age = "15"
+var course = "Web n App Development"
+var institute = "SMIT"
+alert("Name: " + studentName);
+alert("Age: " + age);
+alert("Course: " + course);
+alert("Institute: " + institute);
+
+//_______________________QNO 5
