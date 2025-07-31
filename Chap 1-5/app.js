78f59028351d6b91a4a9e95c6d87442fe7e38d52
Muskaankamran <muskaankamran494@gmail.com>
2025-07-31 09:27:09 -0700

    assignment

--- Chap 1-5/app.js
+++ Chap 1-5/app.js
@@ -1,20 +1,25 @@
+
+
+
+//____________CHAPTER 01_______________
+
 //_____________________QNO 1
 
-alert("Welcome!");
+//alert("Welcome!");
 //____________________QNO 2
 
- alert("Error please enter a valid password")
+ //alert("Error please enter a valid password")
 
 //_____________________QNO 3
 
-alert("Welcome to JS Land \n Happy Coding!");
+//alert("Welcome to JS Land \n Happy Coding!");
 
 //_____________________QNO 4
-alert("Welcome to JS Land..")
-alert("Happy Coding ! \nPrevent this page from creating additional dialogs")
+//alert("Welcome to JS Land..")
+//alert("Happy Coding ! \nPrevent this page from creating additional dialogs")
 
 //____________________QNO 5
-alert("Hello...I can run JS thorugh my web browser's console");
+//alert("Hello...I can run JS thorugh my web browser's console");
 
  //___________________QNO 6
 
@@ -25,29 +30,113 @@ alert("Hello...I can run JS thorugh my web browser's console");
 //__________CHAPTER 02______________
 
 //____________________QNO 1
-var username;
+//var username;
 //___________________QNO 2
-var myName = "Muskaankamran"
-alert(myName);
+//var myName = "Muskaankamran"
+//alert(myName);
 //_________________QNO 3
 
 // a) Declare a JS variable, titled message.
-let message;
+//let message;
 
 // b) Assign “Hello World” to variable message
-message = "Hello World";
+//message = "Hello World";
 
 // c) Display the message in alert box.
-alert(message);
+//alert(message);
 
 //_____________________QNO 4
-var studentName = "Maira"
-var age = "15"
-var course = "Web n App Development"
-var institute = "SMIT"
-alert("Name: " + studentName);
-alert("Age: " + age);
-alert("Course: " + course);
-alert("Institute: " + institute);
+//var studentName = "Maira"
+//var age = "15"
+//var course = "Web n App Development"
+//var institute = "SMIT"
+//alert("Name: " + studentName);
+//alert("Age: " + age);
+//alert("Course: " + course);
+//alert("Institute: " + institute);
 
 //_______________________QNO 5
+//var message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(message);
+
+//__________________________QNO 6
+//var email = "abc@gmail.com"
+//alert("My email address is " + email);
+
+//__________________________QNO 7
+//var book ="A smarter way to learn JavaScript"
+//alert("I am trying to learn from the book A smarter way to learn Javascript "  + book);
+
+//___________________________QNO 8
+//document.write("Yah! I can write HTML content through JavaScript");
+
+//__________________________QNO 9
+// Store the special string in a variable
+//var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// Show in alert box
+//alert(design);
+
+// Show in browser
+//document.write(design);
+
+
+//___________CHAPTER 03_________________
+
+//___________________________QNO 1
+var age = 15;
+alert("I am " + age + "years old");
+
+//____________________________QNO 2
+//var visits = localStorage.getItem("visits");
+
+//if (visits === null) {
+  //visits = 1;} else {
+ // visits = Number(visits) + 1;
+//}
+
+
+//localStorage.setItem("visits", visits);
+
+
+//document.write("You have visited this site " + visits + " times.");
+
+
+//___________________________________QNO 3
+//var birthYear = 2010;
+
+//document.write("My birth year is " + birthYear + "<br>");
+//document.write("Data type of my declared variable is " + typeof(birthYear));
+
+//___________________________________QNO 4
+//var visitorName = "John Doe";
+//var productTitle = "T-shirt";
+//var quantity = 5;
+
+//document.write(visitorName + " ordered " + quantity + "" + productTitle + "(s) on XYZ Clothing store.");
+
+
+
+//____________CHAPTER 04__________________
+
+//________________________________QNO 1
+var name, age, city;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
