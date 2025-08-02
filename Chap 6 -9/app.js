2518a7b958169b8f98ba813354f44b6c88f9e8d0
Muskaankamran <muskaankamran494@gmail.com>
2025-08-02 09:25:03 -0700

    half completed

---
+++ Chap 6 -9/app.js
@@ -0,0 +1,16 @@
+//___CHAPTER MATHEMATICAL EXPRESSIONS____//
+//______________________QNO 1
+var a = 10;
+document.write("Result:<br>");
+  document.write("The value of a is: " + a + "<br><br>");
+  document.write(".............................<br><br>");
+
+    document.write("Result:<br>");
+  document.write("The value of a is: " + a + "<br><br>");
+  document.write("The value of a++ is: " + (a++) + "<br>");
+ document.write("Now the value of a is: " + a + "<br><br>");
+ document.write("The value of --a is: " + (--a) + "<br>");
+ document.write("Now the value of a is: " + a + "<br><br>");
+
+   document.write("The value of a-- is: " + (a--) + "<br>");
+   document.write("Now the value of a is: " + a + "<br>");
