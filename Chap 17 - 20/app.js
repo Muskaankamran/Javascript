7521f557e24b166f271f35f5db7df2ea208dfd02
Muskaankamran <muskaankamran494@gmail.com>
2025-08-29 07:40:16 -0700

    Completed

---
+++ Chap 17 - 20/app.js
@@ -0,0 +1,32 @@
+//....... CHAPTER 17-20.........
+
+//.........................QUESTION 1
+let multiArray = [];
+//.........................QUESTION 2
+// var matrix = [
+//   [0, 1, 2, 3],
+//   [1, 0, 1, 2],
+//   [2, 1, 0, 1]
+// ];
+
+// for (var i = 0; i < matrix.length; i++) {
+//   console.log(matrix[i][0], matrix[i][1], matrix[i][2], matrix[i][3]);
+// }
+//.............................QUESTION 3
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+//...............................QUESTION 4
+
+// let number = prompt("Enter the table number:");
+// let length = prompt("Enter the length of the table:");
+// console.log("Multiplication Table of " + number + ":");
+// for (let i = 1; i <= length; i++) {
+//   console.log(number + " x " + i + " = " + (number * i));
+// }
+//...................................QUESTION 5
+let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
