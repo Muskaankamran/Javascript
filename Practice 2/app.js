5936ef62cbe7c67ffd280f98fbeaa2497d3b6758
Muskaankamran <muskaankamran494@gmail.com>
2025-09-03 12:45:39 -0700

    completed

---
+++ Practice 2/app.js
@@ -0,0 +1,43 @@
+// var x = 5
+// console.log(x += 3);
+// var x = 5;
+// x = x * 2;
+// alert(x);
+
+// var age =prompt("Your age")
+// if (age >= 18) {
+//   alert("Adult");
+// } else {
+//   alert("Minor");
+// }
+
+
+// var score =prompt("Score? ")
+// if (score>=90) {
+//     alert("A")
+// }
+// else if (score>=80) {
+//     alert("B")
+// }
+// else{
+//     alert("You fail the exam! ")
+// }
+
+
+// var name = "Sara";
+// var len = name.length; //4
+
+// var fruits = ["apple", "banana", "cherry"];
+// fruits[1] = "orange";  // ["apple", "orange", "cherry"]
+// fruits.push(strawberry)
+
+var weight =prompt("Weight?? ")
+var time =prompt("time?? ")
+var time =prompt("age?? ")
+var gender=prompt("gender?? ")
+if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+ alert("Come to our tryout!");
+}
+else {
+ alert("Come to our cookout!");
+}
