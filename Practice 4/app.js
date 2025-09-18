ed7cb6b59640f9dc79918e91ba90e41100bd53e3
Muskaankamran <muskaankamran494@gmail.com>
2025-09-18 11:48:43 -0700

    Practice

---
+++ Practice 4/app.js
@@ -0,0 +1,32 @@
+// var str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-+_]:;><?.,|^%$#@!()";
+// var passWord="";
+// for(var i = 0; i<10 ; i++){
+//     var randomNum = Math.floor(Math.random()*str.length)
+//      passWord+=str[randomNum]
+// }
+// console.log(passWord);
+
+
+
+
+
+
+var date = new Date
+var day = date.getDay()
+// console.log(day);
+ var dayNames = ["Sun" ,"mon", "Tues" , "Wed" , "Thurs" , "Fri" ,"Sat"];
+ var today = dayNames[day]
+console.log(today);
+console.log(date.getMonth());
+
+
+
+
+
+
+
+
+
+
+
+
