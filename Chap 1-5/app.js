73ae72bded17b839965a3a95e1c881a76693ec9c
Muskaankamran <muskaankamran494@gmail.com>
2025-10-04 12:54:27 -0700

    completed

---
+++ Chap 1-5/app.js
@@ -0,0 +1,340 @@
+
+
+
+//____________CHAPTER 01_______________
+
+//_____________________QNO 1
+
+//alert("Welcome!");
+//____________________QNO 2
+
+ //alert("Error please enter a valid password")
+
+//_____________________QNO 3
+
+//alert("Welcome to JS Land \n Happy Coding!");
+
+//_____________________QNO 4
+//alert("Welcome to JS Land..")
+//alert("Happy Coding ! \nPrevent this page from creating additional dialogs")
+
+//____________________QNO 5
+//alert("Hello...I can run JS thorugh my web browser's console");
+
+ //___________________QNO 6
+
+ //_______IN index.html file
+
+
+
+//__________CHAPTER 02______________
+
+//____________________QNO 1
+//var username;
+//___________________QNO 2
+//var myName = "Muskaankamran"
+//alert(myName);
+//_________________QNO 3
+
+// a) Declare a JS variable, titled message.
+//let message;
+
+// b) Assign “Hello World” to variable message
+//message = "Hello World";
+
+// c) Display the message in alert box.
+//alert(message);
+
+//_____________________QNO 4
+//var studentName = "Maira"
+//var age = "15"
+//var course = "Web n App Development"
+//var institute = "SMIT"
+//alert("Name: " + studentName);
+//alert("Age: " + age);
+//alert("Course: " + course);
+//alert("Institute: " + institute);
+
+//_______________________QNO 5
+//var message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+//alert(message);
+
+//__________________________QNO 6
+//var email = "abc@gmail.com"
+//alert("My email address is " + email);
+
+//__________________________QNO 7
+//var bookDescription = "I am trying to learn form the Book  "
+//var book = "  A Smarter Way to Learn Javascript";
+//alert(bookDescription + book);
+//___________________________QNO 8
+//document.write("Yah! I can write HTML content through JavaScript");
+
+//__________________________QNO 9
+// Store the special string in a variable
+//var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// Show in alert box
+//alert(design);
+
+// Show in browser
+//document.write(design);
+
+
+//___________CHAPTER 03_________________
+
+//___________________________QNO 1
+//var age = 15;
+//alert("I am " + age + "years old");
+
+//____________________________QNO 2
+//var visit = 2
+//alert("You have visited this site " + visit  +  " times " );
+
+
+//___________________________________QNO 3
+//var birthYear = 2010;
+
+//document.write("My birth year is " + birthYear + "<br>");
+//document.write("Data type of my declared variable is " + typeof(birthYear));
+
+//___________________________________QNO 4
+//var visitorName = "John Doe";
+//var productTitle = "T-shirt";
+//var quantity = 5;
+
+//document.write(visitorName + " ordered " + quantity + "" + productTitle + "(s) on XYZ Clothing store.");
+
+
+
+//____________CHAPTER 04__________________
+
+//________________________________QNO 1
+//var name, age, city;
+//______________________________QNO 2
+//Five legal
+//myVar
+//$price
+//_count
+//userName1
+//total_amount
+//Five illegal
+//1stName
+//my-var
+//@username
+//var
+//user name
+
+//__________CHAPTER 05_____________
+//_____________________QNO 1
+//var num1 =3
+//var num2 =5
+ //alert(num1 + num2)
+//____________________QNO 2
+//SUBTRACTION
+//var num1 = 3;
+//var num2 = 5;
+//alert(num1 - num2)
+//MULTIPLICATION
+//var num1 = 3;
+//var num2 = 5;
+//alert(num1 * num2)
+//DIVISION
+//var num1 = 3;
+//var num2 = 5;
+//alert(num1 / num2)
+//MODULUS
+var num1 = 3;
+var num2 = 5;
+alert(num1 % num2)
+//___________________________QNO 3
+// a. Declare a variable
+var num;
+
+// b. Show the value of variable after declaration
+document.write("Value after variable declaration is " + num + "<br>");
+
+// c. Initialize the variable with some number
+num = 5;
+
+// d. Show the initial value
+document.write("Initial value: " + num + "<br>");
+
+// e. Increment the variable
+num++;
+
+// f. Show the value after increment
+document.write("Value after increment is: " + num + "<br>");
+
+// g. Add 7 to the variable
+num += 7;
+
+// h. Show the value after addition
+document.write("Value after addition is: " + num + "<br>");
+
+// i. Decrement the variable
+num--;
+
+// j. Show the value after decrement
+document.write("Value after decrement is: " + num + "<br>");
+
+// k. Show the remainder after dividing the variable's value by 3
+let remainder = num % 3;
+
+// l. Output the remainder
+document.write("The remainder is: " + remainder + "<br>");
+
+
+//______________________________________QNO 4
+
+var ticketPrice = 600;
+
+var totalCost = ticketPrice * 5;
+
+//alert("Cost of one movie ticket is: " + ticketPrice + " PKR\n" +
+   //   "Total cost to buy 5 tickets to a movie is: " + totalCost + " PKR");
+      //___________________________________QNO 5
+      // Table of 4
+//document.write("Table of 4<br>");
+//document.write("4 x 1 = " + (4 * 1) + "<br>");
+//document.write("4 x 2 = " + (4 * 2) + "<br>");
+//document.write("4 x 3 = " + (4 * 3) + "<br>");
+//document.write("4 x 4 = " + (4 * 4) + "<br>");
+//document.write("4 x 5 = " + (4 * 5) + "<br>");
+//document.write("4 x 6 = " + (4 * 6) + "<br>");
+//document.write("4 x 7 = " + (4 * 7) + "<br>");
+//document.write("4 x 8 = " + (4 * 8) + "<br>");
+//document.write("4 x 9 = " + (4 * 9) + "<br>");
+//document.write("4 x 10 = " + (4 * 10) + "<br>");
+
+
+//_____________________________________QNO 6
+document.write("<h2>Conversion Of Celsius And Fahrenheit</h2>")
+// a)
+var celsius = 25;
+
+// b)
+var fahrenheitFromCelsius = (celsius * 9 / 5) + 32;
+document.write(celsius + "°C is " + fahrenheitFromCelsius + "°F<br>");
+
+// c)  
+var fahrenheit = 71;
+
+// d)
+var celsiusFromFahrenheit = (fahrenheit - 32) * 5 / 9;
+document.write(fahrenheit + "°F is " + celsiusFromFahrenheit + "°C" + "<br>");
+
+
+//___________________________QNO 7
+
+// Prices
+//var price1 = 500;
+//var price2 = 300;
+
+// Quantities
+//var qty1 = 2;
+//var qty2 = 3;
+
+// Shipping charges
+//var shipping = 100;
+
+// Calculate total cost
+//var total = (price1 * qty1) + (price2 * qty2) + shipping;
+
+// Show receipt
+//document.write("<h1>SHOPPING CART</h1>")
+//document.write("Price of item 1: " + price1 + " PKR<br>");
+//document.write("Quantity of item 1: " + qty1 + "<br>");
+//document.write("Price of item 2: " + price2 + " PKR<br>");
+//document.write("Quantity of item 2: " + qty2 + "<br>");
+//document.write("Shipping Charges: " + shipping + " PKR<br><br>");
+//document.write("Total Cost: " + total + " PKR");
+
+
+
+//_____________________QNO 8
+document.write("<h2>MARK SHEET</h2>")
+var totalMarks = 500;
+var marksObtained = 420;
+
+var percentage = (marksObtained / totalMarks) * 100;
+
+// Display the result
+document.write("Total Marks: " + totalMarks + "<br>");
+document.write("Marks Obtained: " + marksObtained + "<br>");
+document.write("Percentage: " + percentage.toFixed(2) + "%");
+
+//_________________________________QNO 9
+document.write("<h2>CURRENCY IN PKR</h2>")
+// Conversion rates and amounts
+var totalInPKR = (10 * 104.80) + (25 * 28);
+
+// Display the result
+document.write("Total Currency in Pakistani Rupees: " + totalInPKR + " PKR");
+//________________________________QNO 10
+document.write("<h2>Arithmetic Operation</h2>")
+var number = 10;
+var result = ((number + 5) * 10) / 2;
+document.write("Initial number: " + number + "<br>");
+document.write("Result after calculation: " + result);
+
+//_____________________________QNO 11
+//   Question 11
+document.write("<h2>Age Calculator</h2>")
+// a.  
+var currentYear = 2016;
+document.write("Current Year: " + currentYear + "<br>")
+
+// b. 
+var birthYear = 1992;
+document.write("Birth Year: " + birthYear + "<br>")
+
+// c.  
+var age1 = currentYear - birthYear;
+var age2 = age1 - 1;
+document.write("Your Age is " + age1 + "<br>");
+
+//______________________________QNO 12
+var radius = 20;
+var pi = 3.142;
+
+// b.  
+var circumference = 2 * pi * radius;
+document.write("Radius of a Circle is " + radius + "<br>");
+document.write("The circumference is " + circumference + "<br>");
+
+// c.  
+var area = pi * radius * radius;
+document.write("The area is " + area);
+
+// Show the results
+alert("The circumference is " + circumference);
+alert("The area is " + area);
+
+//____________________QNO 13
+document.write("<h2>THE LIFETIME SUPPLY CALCULATOR</h2>")
+// a. Favorite snack
+let favoriteSnack = "Chips";
+
+// b. Current age
+let currentAge = 18;
+
+// c. Maximum expected age
+let maxAge = 80;
+
+// d. Estimated amount per day
+let amountPerDay = 2;
+
+// e. Calculate total needed for the rest of life
+let yearsRemaining = maxAge - currentAge;
+let totalNeeded = yearsRemaining * 365 * amountPerDay;
+
+// Output the result
+document.write("You will need " + totalNeeded + " " + favoriteSnack + " to last you until the ripe old age of " + maxAge + ".");
+
+
+
+
+
+
+
---
+++ Class 24/app.js
@@ -0,0 +1,31 @@
+//SWITCH///
+
+// var day=prompt("Enter a day: ");
+// switch(day){
+//     case "sun" :
+//  alert("Its a holiday! ");
+//   break;
+//   case "sat" : 
+//   alert("Its a weekend! ");
+//   break;
+//   case "fri" : 
+//   alert("Its a half day! ");
+//   break;
+//   default:
+//     alert("Its a busy day! ");
+// }
+
+var num1 = prompt("Emter num1: ")
+var num2 = prompt("Emter num2: ");
+var operator = prompt("Emter operator: ");
+switch(operator){
+    case "+" :
+        alert(num1 + num2);
+        break;
+        case "-" :
+            alert(num1 - num2)
+            break;
+            default :alert("error");
+        
+}
+
