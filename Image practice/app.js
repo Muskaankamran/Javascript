c1e3bb69601b4e987f7efae804060052a17d2cb2
Muskaankamran <muskaankamran494@gmail.com>
2025-10-14 18:00:24 +0500

    image practice

---
+++ Image practice/app.js
@@ -0,0 +1,10 @@
+
+
+
+function run(e) {
+    e.src = "https://i.pinimg.com/originals/65/5d/2d/655d2da400a648abca600766a89deead.gif";
+  }
+
+  function change(e) {
+    e.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0UgN1kZsJ2jrmRstxl53z8PGvwz9C8z5uHQ&s";
+  }
