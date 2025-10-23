1dbeac0f3927e96b9c1d15560cc90c7adf80d829
Muskaankamran <muskaankamran494@gmail.com>
2025-10-23 17:50:19 +0500

    form-practice

---
+++ Practice 6/app.js
@@ -0,0 +1,14 @@
+function submit() {
+    var fname = document.getElementById(fname).value
+    var lname = document.getElementById(lname).value
+
+    var heading = document.getElementById(heading)
+
+     
+
+
+}
+
+
+
+
