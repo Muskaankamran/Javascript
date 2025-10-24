4f27ad9091ad3a481341b825d03bbf230b3c5964
Muskaankamran <muskaankamran494@gmail.com>
2025-10-24 17:56:33 +0500

    half completed

--- Chap 49-52/app.js
+++ Chap 49-52/app.js
@@ -1,26 +1,37 @@
-//________________Answer01____________
-// function data(){
-//     var fname = document.getElementById("fname").value
-//     var lname = document.getElementById("lname").value
-//     console.log(fname + " " + lname);
-
-// }
-
+// ________________Answer01____________
+// const form = document.getElementById('signupForm');
+//     const name = document.getElementById('name').value;
+//     const email = document.getElementById('email').value;
+//     const password = document.getElementById('password').value;
+//     form.reset();
 //______________ANSWER02__________________
 
-function expandText() {
-    var shortSent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
-    var text = " Lorem ipsum dolor sit amet consectetur, adipisicing elit.Vel voluptatem, magnam cum reprehenderit mollitia placeat. Impedit voluptatibus iure laborum eos dolor, et ullam molestias debitis itaque quas eligendi reiciendis architecto!"
-  var para = document.getElementById("para")
-  var link = document.getElementById("link")
+// function expandText() {
+//     var shortSent = "Lorem ipsum dolor sit amet consectetur, adipisicing elit."
+//     var text = " Lorem ipsum dolor sit amet consectetur, adipisicing elit.Vel voluptatem, magnam cum reprehenderit mollitia placeat. Impedit voluptatibus iure laborum eos dolor, et ullam molestias debitis itaque quas eligendi reiciendis architecto!"
+//   var para = document.getElementById("para")
+//   var link = document.getElementById("link")
  
-    if(link.innerHTML==="See more..."){
-        link.innerHTML="See less.."
-        para.innerHTML = text
-    }else{
-        link.innerHTML = "See more..."
-        para.innerHTML = shortSent
-    }
+//     if(link.innerHTML==="See more..."){
+//         link.innerHTML="See less.."
+//         para.innerHTML = text
+//     }else{
+//         link.innerHTML = "See more..."
+//         para.innerHTML = shortSent
+//     }
 
     
-}
+// }
+
+
+    //____________ANSWER03________________
+
+    function deleteRow(button) {
+        var row = button.closest('tr');  
+        row.remove();
+      }
+      function editRow(button){
+     
+      }
+  
+
