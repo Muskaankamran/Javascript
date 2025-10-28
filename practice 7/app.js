887c164b6e008f33b4b32fe52856b05dd6dd25f3
Muskaankamran <muskaankamran494@gmail.com>
2025-10-28 18:03:08 +0500

    completed

---
+++ practice 7/app.js
@@ -0,0 +1,18 @@
+// function showList(){
+   
+//    var list = ` 
+//     <ul>
+//     <li>Pizza</li>
+//     <li>Burger</li>
+//     <li>Sandwich</li>
+//     <li>Fries</li>
+// </ul>
+//     `;
+//     var container = document.getElementById("container");
+//     container.innerHTML = list;
+// }
+// function hideList(){
+//     container.innerHTML = " "
+
+// }
+
